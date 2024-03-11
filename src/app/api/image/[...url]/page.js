ba61b91aad3a36cdb6d1bd18c2c3f099e9ee63a5
sh20raj <shaswatraj3@gmail.com>
2024-03-11 21:09:35 +0530

    thats all

--- src/app/api/image/[...url]/page.js
+++ src/app/api/image/[...url]/page.js
@@ -1,22 +1,22 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-// export const GET = async () => {
+// // export const GET = async () => {
 
-//   const res = await fetch("https://picsum.photos/200/300");
-//   const blob = await res.blob();
+// //   const res = await fetch("https://picsum.photos/200/300");
+// //   const blob = await res.blob();
 
-//   const headers = new Headers();
+// //   const headers = new Headers();
 
-//   headers.set("Content-Type", "image/*");
+// //   headers.set("Content-Type", "image/*");
   
-//   return new NextResponse(blob, { status: 200, statusText: "OK", headers });
-// };
+// //   return new NextResponse(blob, { status: 200, statusText: "OK", headers });
+// // };
 
-export  async function GET(req, res) {
-  const img = await fetch("https://picsum.photos/200/300");
-  const blob = await img.blob();
-  const text = await blob.arrayBuffer();
-  res.setHeader("content-Type", "text/plain");
-  const encoded = Buffer.from(text).toString("base64");
-  res.send(encoded);
-}
+// export  async function GET(req, res) {
+//   const img = await fetch("https://picsum.photos/200/300");
+//   const blob = await img.blob();
+//   const text = await blob.arrayBuffer();
+//   res.setHeader("content-Type", "text/plain");
+//   const encoded = Buffer.from(text).toString("base64");
+//   res.send(encoded);
+// }
--- src/app/api/image/page.js
+++ src/app/api/image/page.js
@@ -1,8 +1,8 @@
-export default async function GET(req, res) {
-    const img = await fetch("https://picsum.photos/200/300");
-    const blob = await img.blob();
-    const text = await blob.arrayBuffer();
-    res.setHeader("content-Type", "text/plain");
-    const encoded = Buffer.from(text).toString("base64");
-    res.send(encoded);
-  }
+// export default async function GET(req, res) {
+//     const img = await fetch("https://picsum.photos/200/300");
+//     const blob = await img.blob();
+//     const text = await blob.arrayBuffer();
+//     res.setHeader("content-Type", "text/plain");
+//     const encoded = Buffer.from(text).toString("base64");
+//     res.send(encoded);
+//   }
