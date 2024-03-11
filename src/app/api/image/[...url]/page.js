aeacc414c104b0fe2618fde269391d95e8cee3d0
sh20raj <shaswatraj3@gmail.com>
2024-03-11 08:27:20 +0530

    img

--- src/app/api/image/[...url]/page.js
+++ src/app/api/image/[...url]/page.js
@@ -1,14 +1,15 @@
-function PicsumImage() {
-  return (
-    <div>
-      <img
-        src="https://picsum.photos/200"
-        alt="Picsum Image"
-        width={200}
-        height={200}
-      />
-    </div>
-  );
-}
 
-export default PicsumImage;
+// import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+
+// export const GET = async (_req: NextRequest) => {
+
+//   const res = await fetch("https://picsum.photos/200/300");
+//   const blob = await res.blob();
+
+//   const headers = new Headers();
+
+//   headers.set("Content-Type", "image/*");
+  
+//   return new NextResponse(blob, { status: 200, statusText: "OK", headers });
+// };
--- src/app/api/image/page.js
+++ src/app/api/image/page.js
@@ -1,1 +1,14 @@
-let i;
+function PicsumImage() {
+    return (
+      <div>
+        <img
+          src="https://picsum.photos/200"
+          alt="Picsum Image"
+          width={200}
+          height={200}
+        />
+      </div>
+    );
+  }
+  
+  export default PicsumImage;
