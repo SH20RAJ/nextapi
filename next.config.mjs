2dd8985ae3553702cff014728a6096691c5cd409
sh20raj <shaswatraj3@gmail.com>
2024-03-14 08:54:44 +0530

    *

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         {
           // matching all API routes
           // https://vercel.com/guides/how-to-enable-cors
-          source: "/api/:path*",
+          source: "*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
